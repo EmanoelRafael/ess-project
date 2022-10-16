ae30c648f2b1e1e07dd0b730c194c849130b86bc
EmanoelRafael <emanoelrafael2020@gmail.com>
2022-10-16 00:35:35 -0300

    "Adicionando configuracoes para o sistema"

--- ta-server/ta-server.ts
+++ ta-server/ta-server.ts
@@ -1,11 +1,27 @@
 import express = require('express');
+import bodyParser = require("body-parser");
 
 var taserver = express();
 
-var alunos = [{nome:'Paulo',cpf:'683',email:'phmb@cin.br',metas:{'requisitos':'MA'}},{nome:'Mariana',cpf:'456',email:'@mcb@cin.br',metas:{'requisitos':'MPA'}}];
+var alunos = [{nome:'Paulo',cpf:'nail',email:'phmb@cin.br',metas:{'requisitos':'MA'}},{nome:'Mariana',cpf:'456',email:'@mcb@cin.br',metas:{'requisitos':'MPA'}}];
 
-taserver.get('/', function (req, res) {
-  res.send(alunos);
+var allowCrossDomain = function(req: any, res: any, next: any) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+taserver.use(allowCrossDomain);
+
+taserver.use(bodyParser.json());
+
+taserver.get('/', function (req: express.Request, res: express.Response) {
+  res.send(JSON.stringify(alunos));
+})
+
+taserver.put('/aluno', function (req: express.Request, res: express.Response) {
+  var alunos = Number(req.body);
+  res.send("Mensagem Recebida!");
 })
 
 taserver.listen(3000, function () {
