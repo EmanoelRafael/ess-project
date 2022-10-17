efda47b9864d4f349c451c65f4e1274dc233c3c2
EmanoelRafael <emanoelrafael2020@gmail.com>
2022-10-16 23:51:44 -0300

    "Criando a classe de serviço no front"

--- ta-gui/src/app/cart.component.ts
+++ ta-gui/src/app/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core'
+import { StoreService } from './store.service';
 
 @Component({
     selector: 'cart',
@@ -7,5 +8,8 @@ import { Component, OnInit} from '@angular/core'
 })
 
 export class CartComponent {
+    constructor(private storeService: StoreService) {};
+
+    
     title = 'cart';
 }
---
+++ ta-gui/src/app/store.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { retry, map } from 'rxjs/operators';
+
+@Injectable()
+export class StoreService {
+    private headers = new HttpHeaders({'Content-Type': 'application/json'});
+    private taURL = 'http://localhost:3000';
+
+    constructor(private http: HttpClient) {}
+}
